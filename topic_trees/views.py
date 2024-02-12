93f8e0b76c7252fc73440887c56f747168b6f793
Nader Fares <naderalifares16@hotmail.com>
2024-02-12 14:33:11 -0300

    Creating Topics Route and Connect to Database

--- topic_trees/views.py
+++ topic_trees/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
   # TOPIC TREE INDEX PAGE
   return render(request, 'topic_trees/index.html')
+
+def topics(request):
+  # USER TOPICS PAGE
+  topics = Topic.objects.order_by('date_added')
+  context = {
+    'topics': topics
+  }
+  return render(request, 'topic_trees/topics.html', context)
