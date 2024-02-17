3a151fc7d112210db4eca2819f8fbfbdc9fce84d
Nader Fares <naderalifares16@hotmail.com>
2024-02-17 00:56:16 -0300

    Creating Topic Route

--- topic_trees/views.py
+++ topic_trees/views.py
@@ -12,3 +12,12 @@ def topics(request):
     'topics': topics
   }
   return render(request, 'topic_trees/topics.html', context)
+
+def topic(request, topic_id):
+  topic = Topic.objects.get(id = topic_id)
+  entries = topic.entry_set.order_by('-date_added')
+  context = {
+    'topic': topic,
+    'entries': entries
+  }
+  return render(request, 'topic_trees/topic.html', context)
