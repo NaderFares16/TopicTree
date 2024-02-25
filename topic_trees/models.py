6ac244aae1ed38f6aad42d751d73fac6e73bd429
Nader Fares <naderalifares16@hotmail.com>
2024-02-24 21:56:15 -0300

    feat: Linking an owner to each topic created

--- topic_trees/models.py
+++ topic_trees/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
   # ASSUNTO NA QUAL USUÁRIO ESTÁ APRENDENDO
   text = models.CharField(max_length=200)
   date_added = models.DateTimeField(auto_now_add=True)
+  owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     # RETORNA STRING DO MODELO
